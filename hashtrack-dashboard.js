8ec43dbf0f74dc5f6e74b68d79316614f5ab61d6
Trey Huffine <treyhuffine@gmail.com>
2015-07-23 10:22:12 -0700

    Filter for unique Tweeters

--- hashtrack-dashboard.js
+++ hashtrack-dashboard.js
@@ -3,6 +3,16 @@ Tweets = new Mongo.Collection("tweets");
 var currentTime = new Date().getTime();
 var dayLength = 86400000;
 var previousDay = currentTime - dayLength;
+function unique(arr) {
+    var hash = {}, result = [];
+    for ( var i = 0, l = arr.length; i < l; ++i ) {
+        if ( !hash.hasOwnProperty(arr[i]) ) { //it works with objects! in FF, at least
+            hash[ arr[i] ] = true;
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
 
 if (Meteor.isClient) {
   // Template.body.helpers({
@@ -12,8 +22,19 @@ if (Meteor.isClient) {
   // })
   angular.module('hashtrack', ['angular-meteor']);
   angular.module('hashtrack').controller('DashCtrl', ['$scope', '$meteor',
-    function($scope, $meteor) {
+    function($scope, $meteor, $filter) {
+
       $scope.tweets = $meteor.collection(Tweets);
+
+      $scope.uniqueTweeters = function(tweets) {
+        var allUsers = [];
+        tweets.forEach(function(e, i) {
+          allUsers.push(e.user.screen_name);
+        });
+        var uniqueUsers = unique(allUsers);
+        return uniqueUsers.length;
+      };
+
   }]);
 }
 
