f9744b3df8c87e047dd1e6851832e5e5e060b2a0
Trey Huffine <treyhuffine@gmail.com>
2015-07-23 01:46:11 -0700

    Display results for dashboard

--- hashtrack-dashboard.js
+++ hashtrack-dashboard.js
@@ -1,5 +1,5 @@
-Tweet = new Mongo.Collection("tweets");
-var fullTweets = Tweet.find({});
+Tweets = new Mongo.Collection("tweets");
+// var fullTweets = Tweets.find({});
 var currentTime = new Date().getTime();
 var dayLength = 86400000;
 var previousDay = currentTime - dayLength;
@@ -9,16 +9,11 @@ if (Meteor.isClient) {
   //   tweets: function () {
   //     return Tweet.find({});
   //   }
-  // });
-console.log("meteor");
+  // })
   angular.module('hashtrack', ['angular-meteor']);
   angular.module('hashtrack').controller('DashCtrl', ['$scope', '$meteor',
-    function($scope) {
-      $scope.tweets = [
-        {timestamp_ms: 21231},
-        {timestamp_ms: 77777}
-      ]
-      console.log("in angular");
+    function($scope, $meteor) {
+      $scope.tweets = $meteor.collection(Tweets);
   }]);
 }
 
