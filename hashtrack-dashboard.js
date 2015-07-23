b5cd24ec1595534323dfe6e4cc1f8983f981669a
Trey Huffine <treyhuffine@gmail.com>
2015-07-22 23:01:19 -0700

    Connect meteor to remote database

--- hashtrack-dashboard.js
+++ hashtrack-dashboard.js
@@ -1,23 +1,22 @@
+Tweet = new Mongo.Collection("tweets");
+
+var currentTime = new Date().getTime();
+var dayLength = 86400000;
+var previousDay = currentTime - dayLength;
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    tweets: function () {
+      return Tweet.find({});
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+
+Meteor.methods({
+  filterTweets: function(tweets) {
+    return tweets.filter(function(e, i) {
+      return parseInt(e.timestamp_ms) > previousDay;
+    });
+  }
+});
