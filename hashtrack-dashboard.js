b8a600bd77606add3ef3c6018221953b656b9031
Trey Huffine <treyhuffine@gmail.com>
2015-07-22 23:37:49 -0700

    Load test data correctly

--- hashtrack-dashboard.js
+++ hashtrack-dashboard.js
@@ -10,21 +10,23 @@ if (Meteor.isClient) {
   //     return Tweet.find({});
   //   }
   // });
+console.log("meteor");
   angular.module('hashtrack', ['angular-meteor']);
-  angular.module('hashtrack').controller('dash', ['$scope', '$meteor',
+  angular.module('hashtrack').controller('DashCtrl', ['$scope', '$meteor',
     function($scope) {
       $scope.tweets = [
         {timestamp_ms: 21231},
         {timestamp_ms: 77777}
       ]
-    }]);
+      console.log("in angular");
+  }]);
 }
 
 
-Meteor.methods({
-  filterTweet: function(tweets) {
-    return tweets.filter(function(e, i) {
-      return parseInt(e.timestamp_ms) > previousDay;
-    });
-  }
-});
+// Meteor.methods({
+//   filterTweet: function(tweets) {
+//     return tweets.filter(function(e, i) {
+//       return parseInt(e.timestamp_ms) > previousDay;
+//     });
+//   }
+// });
