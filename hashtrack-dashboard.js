4768bdca62dac67cb1860a9357187f9ec66d4e1a
Trey Huffine <treyhuffine@gmail.com>
2015-07-22 23:28:47 -0700

    Add Angular to meteor

--- hashtrack-dashboard.js
+++ hashtrack-dashboard.js
@@ -1,20 +1,28 @@
 Tweet = new Mongo.Collection("tweets");
-
+var fullTweets = Tweet.find({});
 var currentTime = new Date().getTime();
 var dayLength = 86400000;
 var previousDay = currentTime - dayLength;
 
 if (Meteor.isClient) {
-  Template.body.helpers({
-    tweets: function () {
-      return Tweet.find({});
-    }
-  });
+  // Template.body.helpers({
+  //   tweets: function () {
+  //     return Tweet.find({});
+  //   }
+  // });
+  angular.module('hashtrack', ['angular-meteor']);
+  angular.module('hashtrack').controller('dash', ['$scope', '$meteor',
+    function($scope) {
+      $scope.tweets = [
+        {timestamp_ms: 21231},
+        {timestamp_ms: 77777}
+      ]
+    }]);
 }
 
 
 Meteor.methods({
-  filterTweets: function(tweets) {
+  filterTweet: function(tweets) {
     return tweets.filter(function(e, i) {
       return parseInt(e.timestamp_ms) > previousDay;
     });
