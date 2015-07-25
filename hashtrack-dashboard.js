0df8f1566775ad9b05d03f5da422dbbaf49a7dcc
Trey Huffine <treyhuffine@gmail.com>
2015-07-24 23:01:58 -0700

    Style dashboard

--- hashtrack-dashboard.js
+++ hashtrack-dashboard.js
@@ -3,7 +3,7 @@ Tweets = new Mongo.Collection("tweets");
 function unique(arr) {
     var hash = {}, result = [];
     for ( var i = 0, l = arr.length; i < l; ++i ) {
-        if ( !hash.hasOwnProperty(arr[i]) ) { //it works with objects! in FF, at least
+        if ( !hash.hasOwnProperty(arr[i]) ) {
             hash[ arr[i] ] = true;
             result.push(arr[i]);
         }
@@ -27,12 +27,15 @@ if (Meteor.isClient) {
         return uniqueUsers.length;
       };
 
+      $scope.logTweet = function(tweet) {
+        console.log(tweet);
+      }
+
       $scope.$watch('tweets', function(newVal, oldVal) {
         var currentTime = new Date().getTime();
         var dayLength = 86400000;
         var previousDay = currentTime - dayLength;
         $scope.dailyTweets = $scope.tweets.filter(function(el, idx) {
-          console.log(el.timestamp_ms, Number(el.timestamp_ms));
           return Number(el.timestamp_ms) > previousDay;
         });
       });
